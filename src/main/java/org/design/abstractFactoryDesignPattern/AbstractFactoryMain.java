c55ec1861f4f3a8b5d668b0b36de2f49adfc702d
SaloniCodeLab <saloni987jain@gmail.com>
2024-10-19 02:46:44 +0530

    Factory design Pattern

---
+++ src/main/java/org/design/abstractFactoryDesignPattern/AbstractFactoryMain.java
@@ -0,0 +1,4 @@
+package org.design.abstractFactoryDesignPattern;
+
+public class AbstractFactoryMain {
+}
---
+++ src/main/java/org/design/abstractFactoryDesignPattern/RollerCosterRideFactory.java
@@ -0,0 +1,4 @@
+package org.design.abstractFactoryDesignPattern;
+
+public class RollerCosterRideFactory implements Ride{
+}
